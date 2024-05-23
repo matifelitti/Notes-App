ddc78e7117b650c2af73c057758f8e1f37deeb38
matifelitti <matiasfelitti@gmail.com>
2024-05-23 11:11:29 -0300

    delete card and css changes

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ document.getElementById("form").addEventListener("submit", function (event) {
 
   const cardContainer = document.getElementById("card-container");
   const textarea = document.getElementById("textarea").value;
-  const card = document.createElement("card");
+  const cardId = Date.now();
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.id = cardId;
   let date = new Date().toLocaleString();
   card.innerHTML = `
       <h6>${date}</h6>
       <div>${textarea}</div>
+      <button onclick="deleteCard(${cardId})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+    </svg></button>
     `;
   cardContainer.appendChild(card);
 });
+
+function deleteCard(cardId) {
+  const card = document.getElementById(cardId);
+  card.remove();
+}
